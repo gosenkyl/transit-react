7a0cdd7e65e6b7e2faabca1c31c65b4368553200
Kyle Gosen <gosenkyl@msu.edu>
2017-08-05 23:45:50 -0400

    Scrollable stop times on map

--- src/components/route-header/RouteHeader.js
+++ src/components/route-header/RouteHeader.js
@@ -5,7 +5,6 @@ import './RouteHeader.css';
 export default function RouteHeader(props){
     return (
         <div className="route-header">
-            <div className="route-header-title">Route</div>
             <div className="route-header-short-name">{props.route.routeShortName}</div>
             <div className="route-header-long-name">{props.route.routeLongName}</div>
         </div>
